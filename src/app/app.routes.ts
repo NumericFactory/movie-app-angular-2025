ee667eb2b633249bd35e5050899cc05d47b34744
Frederic Lossignol <frederic.lossignol@gmail.com>
2025-03-17 15:12:11 +0100

    feat : definition d'une route dynamique

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { MovieListPageComponent } from './pages/movie-list-page/movie-list-page.component';
 import { SearchMoviesComponent } from './pages/search-movies/search-movies.component';
+import { MovieDetailPageComponent } from './pages/movie-detail-page/movie-detail-page.component';
 
 export const routes: Routes = [
 
   // definir des routes ici
   { path: '', component: MovieListPageComponent },
+  { path: 'detail/:title', component: MovieDetailPageComponent },
   { path: 'search', component: SearchMoviesComponent }
 
 ];
