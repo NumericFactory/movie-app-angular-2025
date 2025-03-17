d3deb15107003a06823f8d1bb86e90a9ddc2fbc3
Frederic Lossignol <frederic.lossignol@gmail.com>
2025-03-17 14:34:37 +0100

    feat : definition de 2  routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MovieListPageComponent } from './pages/movie-list-page/movie-list-page.component';
+import { SearchMoviesComponent } from './pages/search-movies/search-movies.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  // definir des routes ici
+  { path: '', component: MovieListPageComponent },
+  { path: 'search', component: SearchMoviesComponent }
+
+];
