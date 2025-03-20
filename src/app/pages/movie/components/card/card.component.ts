ebfe85d01e85e41e41857d0c63aceaf0e4952802
NumericFactory <frederic.lossignol@gmail.com>
2025-03-20 22:00:20 +0100

    fix : optimisation image size in card component

--- src/app/pages/movie/components/card/card.component.ts
+++ src/app/pages/movie/components/card/card.component.ts
@@ -3,10 +3,11 @@ import { Movie } from '../../../../shared/models/movie.model';
 import { RouterLink } from '@angular/router';
 import { StarsComponent } from '../../../../ui/stars/stars.component';
 import { ButtonComponent } from "../../../../ui/button/button.component";
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'ui-card',
-  imports: [StarsComponent, RouterLink, ButtonComponent],
+  imports: [StarsComponent, RouterLink, ButtonComponent, NgOptimizedImage],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
