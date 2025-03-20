56e4b7ebdefaafe0699d4bf60ffa63592e54338d
NumericFactory <frederic.lossignol@gmail.com>
2025-03-20 10:59:19 +0100

    feat: searchinput to formControl

--- src/app/pages/search-movies/search-movies.component.ts
+++ src/app/pages/search-movies/search-movies.component.ts
@@ -2,10 +2,11 @@ import { Component, computed, effect, inject, signal } from '@angular/core';
 import { MovieService } from '../../services/movie.service';
 import { RouterLink } from '@angular/router';
 import { CardComponent } from '../movie-list-page/components/card/card.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-movies',
-  imports: [CardComponent],
+  imports: [CardComponent, ReactiveFormsModule],
   templateUrl: './search-movies.component.html',
   styleUrl: './search-movies.component.css'
 })
@@ -14,8 +15,15 @@ export class SearchMoviesComponent {
   movieService = inject(MovieService);
   foundMovies = this.movieService.foundMovies;
 
+  // déclare un objet de la class FormControl
+  searchInput = new FormControl('');
+
   constructor() {
-    console.log(this)
+    console.log(this);
+    // je m'abonne à la source de donnée "input" (le champ de saisie)
+    this.searchInput.valueChanges.subscribe(
+      searchText => console.log(searchText)
+    )
   }
 
   searchMoviesAction(searchText: string): void {
