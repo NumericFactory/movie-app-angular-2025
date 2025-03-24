87055f55f45dea374505b231352a9a5c41189bfd
NumericFactory <frederic.lossignol@gmail.com>
2025-03-24 14:35:53 +0100

    style : add routerLinkActive directive. Role : add CSS class named "active" on navbar links to manage style on routing

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { Component, inject, Signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
